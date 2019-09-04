cda8d453f342ac55669c233a3c96df64d2f73d5e
Alexander Litvinenko <a.litvinenko@synergetica.co>
2019-09-04 21:13:47 +0300

    fix APIException on login

---
+++ src/main/java/com/kaltura/client/types/APIExceptionCode.java
@@ -0,0 +1,9 @@
+package com.kaltura.client.types;
+
+/**
+ * Created by alex_lytvynenko on 04.09.2019.
+ */
+public class APIExceptionCode {
+
+    public static final String ACTION_FORBIDDEN = "500052";
+}
